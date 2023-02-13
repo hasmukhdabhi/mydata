5fb63c80c002fa8da2b5a3c997f76a53ee0e7b6d
hasmukhdabhi <skey.hasmukh@gmail.com>
2023-02-13 09:18:08 +0530

    crud-plugin.php

--- html/exp.php
+++
@@ -1,6 +0,0 @@
-<form action="" method="post" enctype="multipart/form-data">
-    <label for="file">file:</label>
-    <input type="file" id="filetoUplod" name="filetoUplod" ><br><br>
-    <input type="submit" name="submit">
-    <!-- <dfn>html</dfn> -->
-</form>
